a4ac90a617bc3725b5c9f0b537b182596deb744a
Naoto <naotojacob@gmail.com>
2025-08-21 00:17:49 -0600

    slideUpAndFade on scroll successfully added

---
+++ sectionObserver.js
@@ -0,0 +1,16 @@
+// JS file to trigger the 'slideUpAndFade' animation
+// as the user scrolls into each section of the website
+
+const sections = document.querySelectorAll('.fade-section');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const fadeItems = entry.target.querySelectorAll('.fade'); //selects children of the section with fade class
+            fadeItems.forEach(item => item.classList.add('visible'));
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.2 });
+
+sections.forEach(sec => observer.observe(sec));

3a12e708c54b0800986f34c869bca6548ae60e4a
Naoto <naotojacob@gmail.com>
2025-08-27 17:26:00 -0600

    wrap text in a span, do textcontent change there

--- updateFileUpload.js
+++ updateFileUpload.js
@@ -1,12 +1,13 @@
 /* This js file updates the text content of the custom file upload button with the 
 filename of the uploaded file*/
 
-const button = document.querySelector('.custom-file-upload__btn-text');
+const button = document.querySelector('.custom-file-upload');
+const buttonText = document.querySelector('.custom-file-upload__btn-text');
 const hiddenFileInput = document.getElementById('reference-photos');
 
 hiddenFileInput.addEventListener("change", () => {
     if (hiddenFileInput.files.length > 0) {
-        button.textContent = hiddenFileInput.files[0].name;
+        buttonText.textContent = hiddenFileInput.files[0].name;
         button.style.border = "3px solid var(--rose)";
     } else {
         label.textContent = "UPLOAD REFERENCE PHOTOS";

4c24e4b02f35f1db049f694a1cc4ba0338a7e18c
Naoto <naotojacob@gmail.com>
2025-08-27 17:01:37 -0600

    removed references to animation speed

---
+++ updateFileUpload.js
@@ -0,0 +1,5 @@
+/* This js file updates the text content of the custom file upload button with the 
+filename of the uploaded file*/
+
+const button = document.querySelector('.custom-file-upload');
+const hiddenFileInput = document.getElementById('reference-photos');

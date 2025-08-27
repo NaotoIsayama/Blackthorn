149fee2615c5ebf027a149646952f40f203f8e73
Naoto <naotojacob@gmail.com>
2025-08-27 17:05:38 -0600

    testing updateFileUpload.js

--- updateFileUpload.js
+++ updateFileUpload.js
@@ -3,3 +3,11 @@ filename of the uploaded file*/
 
 const button = document.querySelector('.custom-file-upload');
 const hiddenFileInput = document.getElementById('reference-photos');
+
+hiddenFileInput.addEventListener("change", () => {
+    if (input.files.length > 0) {
+        button.textContent = input.files[0].name;
+    } else {
+        label.textContent = "UPLOAD REFERENCE PHOTOS";
+    }
+});

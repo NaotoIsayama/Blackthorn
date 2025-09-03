8193afb929f49a8762c431c264b9e21bde4e9ed5
Naoto <naotojacob@gmail.com>
2025-09-02 22:14:49 -0600

    Add navbar changing bg color

--- sectionObserver.js
+++ sectionObserver.js
@@ -2,8 +2,11 @@
 // as the user scrolls into each section of the website
 
 const sections = document.querySelectorAll('.fade-section');
+const lightsections = document.querySelectorAll('.light-section');
+const nav = document.querySelector('#desktopNav');
+const navHeight = nav.offsetHeight;
 
-const observer = new IntersectionObserver((entries, observer) => {
+const observer1 = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             const fadeItems = entry.target.querySelectorAll('.fade'); //selects children of the section with fade class
@@ -13,9 +16,26 @@ const observer = new IntersectionObserver((entries, observer) => {
     });
 }, { threshold: 0.2});
 
-window.addEventListener('resize', () => {
-    sections.forEach(section => observer.unobserve(section));
-    sections.forEach(section => observer.observe(section));
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach(entry =>{
+        if (entry.isIntersecting && window.scrollY > 0) {
+            nav.classList.add('bg-dark');
+        }
+        else {
+            nav.classList.remove('bg-dark');
+        }
+    })
+}, { 
+    root: null,
+    threshold: 0.1
 });
 
-sections.forEach(sec => observer.observe(sec));
+window.addEventListener('resize', () => {
+    sections.forEach(section => observer1.unobserve(section));
+    sections.forEach(section => observer1.observe(section));
+    lightsections.forEach(section => observer2.unobserve(section));
+    lightsections.forEach(section => observer2.observe(section));
+});
+
+sections.forEach(sec => observer1.observe(sec));
+lightsections.forEach(sec => observer2.observe(sec));

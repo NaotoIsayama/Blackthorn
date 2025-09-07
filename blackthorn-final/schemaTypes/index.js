ae47db9b7eaeb7473ef3b50ee2982f311b1c5589
Naoto <naotojacob@gmail.com>
2025-09-07 03:21:12 -0600

    sanity setup

---
+++ blackthorn-final/schemaTypes/index.js
@@ -0,0 +1,5 @@
+import roadTrip from "./roadTrip"
+
+export const schemaTypes = [
+    roadTrip
+]
---
+++ blackthorn-final/schemaTypes/roadTrip.js
@@ -0,0 +1,10 @@
+export default {
+    name: 'roadTrip',
+    title: 'Road Trip',
+    type: 'object',
+    fields: [
+        { name: 'city', title: 'City', type: 'string'},
+        { name: 'startDate', title: 'Start Date', type: 'date'},
+        { name: 'endDate', title: 'End Date', type: 'date'}
+    ]
+}

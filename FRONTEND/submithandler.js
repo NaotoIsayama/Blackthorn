b3745b1406e47cfcbef61ee32792d796b01bc718
Naoto <naotojacob@gmail.com>
2025-09-16 04:25:14 -0600

    update dev message

--- FRONTEND/submithandler.js
+++ FRONTEND/submithandler.js
@@ -1,10 +1,12 @@
 // DOM elements
 const form = document.querySelector('form');
 const message = document.querySelector('#submit-message');
+const submit = document.getElementById('submit-button')
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    submit.textContent("Submission Disabled!")
+    /*
     form.classList.add('submit-fade-out');
-    setTimeout(()=>{message.classList.add('submit-fade-in');}, 1000);
-    
+    setTimeout(()=>{message.classList.add('submit-fade-in');}, 1000);*/
 }); 

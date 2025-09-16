dcd87bd01d139213c0985fb2a533bd52863bcdfe
Naoto <naotojacob@gmail.com>
2025-09-16 04:00:42 -0600

    filename updates

--- FRONTEND/updateFileUpload.js
+++ FRONTEND/updateFileUpload.js
@@ -1,10 +1,36 @@
 /* This js file updates the text content of the custom file upload button with the 
 filename of the uploaded file*/
 
-const button = document.querySelector('.custom-file-upload-1');
-const buttonText = document.querySelector('.custom-file-upload__btn-text-1');
-const hiddenFileInput = document.getElementById('reference-photos1');
+const button = document.querySelectorAll('.custom-file-upload-1', '.custom-file-upload-2', '.custom-file-upload-3');
+const buttonText = document.querySelectorAll('.custom-file-upload__btn-text-1', '.custom-file-upload__btn-text-1', '.custom-file-upload__btn-text-1');
+const hiddenFileInputs = document.querySelectorAll('#reference-photos1', '#reference-photos2', '#reference-photos3');
 
+// note that all are the same size, so use i to find corresponding elements
+const buttonArray = [...button];
+const buttonTextArray = [...buttonText];
+const hiddenFileInputsArray = [...hiddenFileInputs]
+
+for (let i = 0; i < buttonArray.length; i++) {
+    hiddenFileInputs[i].addEventListener("change", () => {
+        if (hiddenFileInputs[i].files.length > 0) {
+            buttonTextArray[i].textContent = hiddenFileInputs[i].files[0].name;
+            buttonArray[i].style.border = "3px solid var(--rose)";
+        } else {
+            label.textContent = "UPLOAD REFERENCE PHOTOS";
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
+
+/*
 hiddenFileInput.addEventListener("change", () => {
     if (hiddenFileInput.files.length > 0) {
         buttonText.textContent = hiddenFileInput.files[0].name;
@@ -12,4 +38,4 @@ hiddenFileInput.addEventListener("change", () => {
     } else {
         label.textContent = "UPLOAD REFERENCE PHOTOS";
     }
-});
+});*/

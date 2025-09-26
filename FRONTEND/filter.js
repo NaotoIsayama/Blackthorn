76615857f50a985839b0647b32e1a99308d9cee4
Naoto <naotojacob@gmail.com>
2025-09-26 14:35:09 -0600

    begin categorization program

---
+++ FRONTEND/filter.js
@@ -0,0 +1,29 @@
+// Attach a event listener to each button, clear all displays
+// then use the data-attribute of each button to show the right one
+
+
+categoryBtns = document.querySelectorAll('button');
+categories = document.querySelectorAll('.store__category-1, .store__category-2, .store__category-3');
+categoriesArray = Array.from(categories);
+
+categoryBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        clearActive(); //function to clear all categories of the '.active' css class
+
+        console.log('categoriesArray is: ', categoriesArray);
+        console.log('one of the btnCategories is: ', btn.dataset.btnCategory);
+
+        selectedCategory = categoriesArray.find(
+            c => c.classList.contains(btn.dataset.btnCategory)
+        ); 
+        
+        console.log('Selected Category is: ', selectedCategory);
+
+        selectedCategory.classList.add('active') //Add the '.active' css class to selected category
+    })
+})
+
+function clearActive() {
+    categoriesArray.forEach(cat => cat.classList.remove('active')); // Set all categories to display none
+}

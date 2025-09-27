6c0a666c975fdf48e0cb5642b16734b4cc160f8e
Naoto <naotojacob@gmail.com>
2025-09-27 12:29:53 -0600

    format form with placeholders

--- FRONTEND/buildFlashForm.js
+++ FRONTEND/buildFlashForm.js
@@ -1,5 +1,15 @@
-// This program builds a flashForm.html page from metadata saved to sessionStorage
+// This program builds a flashForm.html page on load from metadata saved to sessionStorage
+
+
+/* Notes: In the html, placement will be stored as a string delimited by commas. In JS, you're
+going to have to turn it back into an array manually, or find a way to work with it as is*/
 
 document.addEventListener('DOMContentLoaded', () =>{
+    
+    // Grab placeholders from the DOM
+
+    // Console.log session
     console.log('The Content of session storage is: ', sessionStorage);
+
+
 });
